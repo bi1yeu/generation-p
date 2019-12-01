7ae60d8f3c2a815b32c2d1c4373dfd73df9ef70c
Matthew Bilyeu <bilyeu.matt@gmail.com>
2019-11-30 23:00:14 -0800

    starting crossover work

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [
                  [org.clojure/clojure "1.10.0"]
-                 [org.apache.commons/commons-lang3 "3.9"]
                  ]
   :main ^:skip-aot generation-t.core
   :target-path "target/%s"
--- src/generation_t/core.clj
+++ src/generation_t/core.clj
@@ -6,98 +6,179 @@
    (java.awt.geom AffineTransform)
    (javax.imageio ImageIO)))
 
-(comment
+(def ^:const display-scale-factor 10.0)
+(def ^:const px-width 50)
+(def ^:const px-height 50)
+(def ^:const num-channels 3)
 
-  ;; master palette
-  ;; https://www.pixilart.com/art/flejas-master-palette-8ddc4903c049d7a
-  ;; https://lospec.com/palette-list/fleja-master-palette
-  (def palette [[0x1f 0x18 0x33]
-                [0x2b 0x2e 0x42]
-                [0x41 0x48 0x59]
-                [0x68 0x71 0x7a]
-                [0x90 0xa1 0xa8]
-                [0xb6 0xcb 0xcf]
-                [0xff 0xff 0xff]
-                [0xfc 0xbf 0x8a]
-                [0xb5 0x80 0x57]
-                [0x8a 0x50 0x3e]
-                [0x5c 0x3a 0x41]
-                [0xc9 0x30 0x38]
-                [0xde 0x6a 0x38]
-                [0xff 0xad 0x3b]
-                [0xff 0xe5 0x96]
-                [0xfc 0xf9 0x60]
-                [0xb4 0xd6 0x45]
-                [0x51 0xc4 0x3f]
-                [0x30 0x9c 0x63]
-                [0x23 0x6d 0x7a]
-                [0x26 0x4f 0x6e]
-                [0x23 0x36 0x63]
-                [0x41 0x72 0x91]
-                [0x4c 0x93 0xad]
-                [0x63 0xc2 0xc9]
-                [0x94 0xd2 0xd4]
-                [0xb8 0xfd 0xff]
-                [0x3c 0x29 0x40]
-                [0x46 0x27 0x5c]
-                [0x82 0x64 0x81]
-                [0xf7 0xa4 0x8b]
-                [0xc2 0x71 0x82]
-                [0x85 0x2d 0x66]])
+;; https://lospec.com/palette-list/japanese-woodblock
+(def ^:const palette--japanesewoodblock
+  [[0x2b 0x28 0x21]
+    [0x62 0x4c 0x3c]
+    [0xd9 0xac 0x8b]
+    [0xe3 0xcf 0xb4]
+    [0x24 0x3d 0x5c]
+    [0x5d 0x72 0x75]
+    [0x5c 0x8b 0x93]
+    [0xb1 0xa5 0x8d]
+    [0xb0 0x3a 0x48]
+    [0xd4 0x80 0x4d]
+    [0xe0 0xc8 0x72]
+    [0x3e 0x69 0x58]])
 
-  ;; via https://stackoverflow.com/a/125013/1181141
-  (defn get-image-from-array [pixels width height]
+(def ^:const palette palette--japanesewoodblock)
+
+(defn- vec->image
+  ([pixel-vec]
+    ;; assume square image
+    (let [dim (-> pixel-vec
+                  count
+                  (/ 3)
+                  Math/sqrt
+                  int)]
+      (def dim dim)
+      (vec->image pixel-vec dim dim)))
+  ([pixel-vec width height]
+   ;; via https://stackoverflow.com/a/125013/1181141
     (let [image  (BufferedImage. width height BufferedImage/TYPE_INT_RGB)
           raster (.getRaster image)]
-      (.setPixels ^WritableRaster raster 0 0 width height pixels)
-      image))
+      (.setPixels ^WritableRaster raster 0 0 width height (int-array pixel-vec))
+      image)))
 
-  (get-image-from-array (int-array (flatten a)) 64 64)
+(defn- random-vec [width height]
+  (-> (* width height)
+      (take (repeatedly (partial rand-nth palette)))
+      flatten))
 
-  (defn random-img [width height]
-    (-> (* width height)
-        (take (repeatedly (partial rand-nth palette)))
-        flatten
-        int-array
-        (get-image-from-array width height)))
+(defn- random-vec-2d [width height]
+  (vec
+    (for [i (range height)]
+      (-> width
+          (take (repeatedly (partial rand-nth palette)))
+          flatten
+          vec))))
 
-  (def img (random-img 64 64))
+(defn- random-img [width height]
+  (-> (random-vec width height)
+      (vec->image width height)))
 
-  (defn- save-img [img-name ^BufferedImage img]
-    (ImageIO/write img "png" (File. (str "output/"
-                                         img-name
-                                         ".png"))))
+(defn- solid-gray-vec [width height val]
+  (repeat (* width height num-channels) val))
 
-  (def ^:const display-scale-factor 10.0)
+(defn- black-vec [width height]
+  (solid-gray-vec width height 0))
 
-  ;; https://stackoverflow.com/a/4216635
-  (defn- scale-up [^BufferedImage bi]
-    (let [width    (* display-scale-factor (.getWidth bi))
-          height   (* display-scale-factor (.getHeight bi))
-          after    (BufferedImage. width height BufferedImage/TYPE_INT_RGB)
-          xform    (AffineTransform.)
-          _        (.scale xform display-scale-factor display-scale-factor)
-          scale-op (AffineTransformOp. xform AffineTransformOp/TYPE_NEAREST_NEIGHBOR)]
-      (.filter scale-op bi after)))
+(defn- white-vec [width height]
+  (solid-gray-vec width height 255))
 
-  (save-img "orig" img)
-  (save-img "test" (scale-up img))
+(defn- save-img [img-name ^BufferedImage img]
+  (ImageIO/write img "png" (File. (format "output/%s.png" img-name))))
 
-  )
+;; https://stackoverflow.com/a/4216635
+(defn- scale-up [^BufferedImage bi]
+  (let [width    (* display-scale-factor (.getWidth bi))
+        height   (* display-scale-factor (.getHeight bi))
+        after    (BufferedImage. width height BufferedImage/TYPE_INT_RGB)
+        xform    (AffineTransform.)
+        _        (.scale xform display-scale-factor display-scale-factor)
+        scale-op (AffineTransformOp. xform AffineTransformOp/TYPE_NEAREST_NEIGHBOR)]
+    (.filter scale-op bi after)))
 
-(defn- rand-tweet []
-  (org.apache.commons.lang3.RandomStringUtils/randomAscii 280))
+(def vec0 (random-vec px-width px-height))
 
-;; TODO k-point crossover?
-;; TODO more than two parents?
-(defn- crossover [parent0 parent1]
-  (apply str
-         (for [i (range 280)]
-           (if (= 0 (rand-int 2))
-             (nth (vec parent0) i)
-             (nth (vec parent1) i)))))
+(def vec1 (random-vec px-width px-height))
+
+(def vecblack (black-vec px-width px-height))
+
+(def vecwhite (white-vec px-width px-height))
+
+(save-img "vec0" (scale-up (vec->image (flatten (random-vec-2d 50 50)))))
+
+(save-img "vecblack" (scale-up (vec->image (flatten (vec (map vec (partition 50 vecblack)))))))
+
+(let [n 5]
+  (save-img (str "patched-" n) (scale-up (vec->image (flatten (crossover-2 n
+                                                                           50
+                                                                           50
+                                                                    (vec (map vec (partition (* 3 50) vecblack)))
+                                                                    (vec (map vec (partition (* 3 50) vecwhite)))))))))
+
+;; like k-point crossover, but instead of k points, (randomly) crossover after
+;; runs of length n
+(defn- crossover [n parent0 parent1]
+  (let [parent0-partitioned (partition (* n num-channels) parent0)
+        parent1-partitioned (partition (* n num-channels) parent1)]
+    (flatten
+     (for [i (range (count parent0-partitioned))]
+       (if (= 0 (rand-int 2))
+         (nth parent0-partitioned i)
+         (nth parent1-partitioned i))))))
+
+(defn- patches [n width height]
+  ;; get the coords of n patches that evenly cover a width x height 2D array,
+  ;; taking num-channels into account
+  ;; e.g.
+  ;;
+  ;; an image that is 2x2 will have a width of 6 (2 pixels * 3 color channels)
+  ;;
+  ;; (patches 1 6 2)
+  ;;
+  ;; (([0 0] [0 1] [0 2])  ;; first patch is nw pixel
+  ;;  ([0 3] [0 4] [0 5])  ;; second patch is ne pixel
+  ;;  ([1 0] [1 1] [1 2])  ;; third patch is sw pixel
+  ;;  ([1 3] [1 4] [1 5])) ;; fourth patch is se pixel
+
+  (let [partitioned-indices-i (partition n (range height))
+        partitioned-indices-j (partition (* n num-channels) (range width))]
+    (for [patch-i (range (count partitioned-indices-i))
+          patch-j (range (count partitioned-indices-j))]
+      (for [i (nth partitioned-indices-i patch-i)
+            j (nth partitioned-indices-j patch-j)]
+        [i j]))))
+
+(def patches-memo (memoize patches))
+
+(defn- reshape [flattened-arr width]
+  (vec (map vec (partition (* num-channels width) flattened-arr))))
+
+(defn- crossover-2 [n width height parent0 parent1]
+  ;; loop over the image, patch by patch, taking a given patch from either
+  ;; parent randomly
+  ;; note: vector dimensions ideally are evenly divided by n
+  (let [parent0-2d (reshape parent0 width)
+        parent1-2d (reshape parent1 width)]
+    (flatten
+     (loop [[patch & rest-patches] (patches-memo n
+                                                 (count (first parent0))
+                                                 (count parent0))
+            img                    parent0]
+       (if patch
+         (recur
+          rest-patches
+          (let [src-img (if (= 0 (rand-int 2)) parent0 parent1)]
+            (reduce
+             (fn [res-img pixel]
+               (->> (get-in src-img pixel)
+                    (assoc-in res-img pixel)))
+             img
+             patch)))
+         img)))))
 
 (comment
+
+
+
+  (def parent0 (vec (map vec (partition (* num-channels 50) vecblack))))
+  (def parent1 (random-vec-2d 50 50))
+
+  (count (first parent1))
+
+  (count (patches k (count (first parent0)) (count parent0)))
+
+  (patches 10
+           (count (first parent0))
+           (count parent0))
+
 
   (reduce (fn [acc el]
             (println acc)
@@ -114,8 +195,7 @@
 ;; Genetic Algorithm Tweet Bot
 ;;
 ;; The population is initially seeded with N randomly generated tweets
-;; A chromosome is represented as 280 ASCII characters
-;;   - should it use all utf-8 characters? only certain subset?
+;; An individual is represented as a 64x64 RGB image
 ;; Uniform crossover https://en.wikipedia.org/wiki/Crossover_(genetic_algorithm)#Uniform_crossover
 ;;   - would a k-point crossover produce more interesting output?
 ;; Fitness function is based on likes and retweets
