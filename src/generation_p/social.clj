ff5199a37998f577028dcbd8962b040073d6b49e
Matthew Bilyeu <bilyeu.matt@gmail.com>
2019-12-19 23:36:51 -0800

    dont actually interact w/ twitter

--- src/generation_p/social.clj
+++ src/generation_p/social.clj
@@ -6,33 +6,40 @@
             [twitter.request :as tw.request]))
 
 ;; via https://github.com/adamwynne/twitter-api
-
 (def ^:const ^:private retweet-to-favorite-weighting 30)
 
+;; TODO make a testing env or something
+(def mock-twitter-api true)
+
 ;; https://twitter.com/generationp3
-(def creds (tw.oauth/make-oauth-creds (System/getenv "CONSUMER_KEY")
-                                      (System/getenv "CONSUMER_SECRET")
-                                      (System/getenv "ACCESS_TOKEN")
-                                      (System/getenv "SECRET_KEY")))
+(def ^:private creds
+  (tw.oauth/make-oauth-creds (System/getenv "CONSUMER_KEY")
+                             (System/getenv "CONSUMER_SECRET")
+                             (System/getenv "ACCESS_TOKEN")
+                             (System/getenv "SECRET_KEY")))
 
 (defn debut [individual]
-  (let [filename   (image/save-individual-as-image individual)
-        status-msg (format "gen %d. %s"
-                           (::m/generation-num individual)
-                           (::m/id individual))
-        ;; note: the media-upload-chunked function in the docs isn't published
-        status     (tw.api/statuses-update-with-media
-                    :oauth-creds
-                    creds
-                    :body [(tw.request/file-body-part filename)
-                           (tw.request/status-body-part status-msg)])]
-    (-> status :body :id)))
+  (if mock-twitter-api
+    (rand-int 100000)
+    (let [filename   (image/save-individual-as-image individual)
+          status-msg (format "gen %d. %s"
+                             (::m/generation-num individual)
+                             (::m/id individual))
+          ;; note: the media-upload-chunked function in the docs isn't published
+          status     (tw.api/statuses-update-with-media
+                      :oauth-creds
+                      creds
+                      :body [(tw.request/file-body-part filename)
+                             (tw.request/status-body-part status-msg)])]
+      (-> status :body :id))))
 
 (defn get-fitness [individual]
-  (let [status (-> (tw.api/statuses-show-id :oauth-creds
-                                            creds
-                                            :params {:id (::m/social-id individual)})
-                   :body)]
-    (+ (* retweet-to-favorite-weighting
-          (:retweet_count status))
-       (:favorite_count status))))
+  (if mock-twitter-api
+    (rand-int 20)
+    (let [status (-> (tw.api/statuses-show-id :oauth-creds
+                                              creds
+                                              :params {:id (::m/social-id individual)})
+                     :body)]
+      (+ (* retweet-to-favorite-weighting
+            (:retweet_count status))
+         (:favorite_count status)))))
